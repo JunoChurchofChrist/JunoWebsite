cddd1732c1d1766a8716351ba639af0cbaa1ed6b
Evan Vance <72946268+evandvance@users.noreply.github.com>
2023-12-31 17:48:12 -0700

    Edited footer style

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -1,21 +1,24 @@
 import React from 'react';
 import { FaFacebookF } from 'react-icons/fa';
 import Link from 'next/link';
+import CenterText from '../CenterText';
 
 const Footer = () => {
   return (
-    <div className="px-5 min-w-screen flex justify-around items-baseline">
-      <Link href="/">Juno Church of Christ</Link>
-      <div className="w-1/6 flex justify-around items-center">
-        <Link href="/connect">Contact Us!</Link>
-        <a
-          href="https://www.facebook.com/JunoChurchOfChrist/?ref=br_rs"
-          target="_blank"
-        >
-          <FaFacebookF />
-        </a>
+    <CenterText className="px-5 h-[5vh] min-w-screen">
+      <div className="flex w-full justify-between items-baseline">
+        <Link href="/">Juno Church of Christ</Link>
+        <div className="w-1/6 flex justify-around items-center">
+          <Link href="/connect">Contact Us!</Link>
+          <a
+            href="https://www.facebook.com/JunoChurchOfChrist/?ref=br_rs"
+            target="_blank"
+          >
+            <FaFacebookF />
+          </a>
+        </div>
       </div>
-    </div>
+    </CenterText>
   );
 };
 
