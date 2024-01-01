a5459aeb4d79163ff5020655dc6b88a5d83084e5
Evan Vance <72946268+evandvance@users.noreply.github.com>
2023-12-31 19:57:06 -0700

    Update to file structure for cleaner naming schemes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
-import NavBar from './components/Navbar/NavBar';
+import NavBar from './components/Navbar';
 import Footer from './components/Footer';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -20,7 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <NavBar />
-        <main className="min-h-[85vh]">{children}</main>
+        <main className="min-h-[80vh]">{children}</main>
         <Footer />
       </body>
     </html>
