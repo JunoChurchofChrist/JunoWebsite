7511311fbe8ae66a265decfada9adea6d19671c6
Evan Vance <72946268+evandvance@users.noreply.github.com>
2023-12-31 17:12:22 -0700

    Added Footer Component

---
+++ app/components/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Link from 'next/link';
+
+const Footer = () => {
+  return (
+    <div className="flex justify-between">
+      <Link href="/">Juno Church of Christ</Link>
+      <div></div>
+    </div>
+  );
+};
+
+export default Footer;
