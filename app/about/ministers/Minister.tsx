919c8d6e6b6fe61830b882c58b9ea1e9a391190b
Evan Vance <72946268+evandvance@users.noreply.github.com>
2024-01-03 09:59:53 -0600

    Fixed Problems with minister page (#3)

--- app/about/ministers/Minister.tsx
+++ app/about/ministers/Minister.tsx
@@ -7,15 +7,23 @@ interface MinisterProps {
   children?: ReactNode;
   imgData: StaticImageData;
   title: string;
+  name: string;
 }
 
-const Minister = ({ className, children, imgData, title }: MinisterProps) => {
+const Minister = ({
+  className,
+  children,
+  imgData,
+  title,
+  name,
+}: MinisterProps) => {
   return (
-    <CenterText className={'flex-col m-5 text-2xl p-5' + className}>
+    <CenterText className={'flex-col m-5 text-2xl p-5 min-w-[80%]' + className}>
       <h2 className="font-bold my-2">{title}</h2>
+      <h3 className="my-2">{name}</h3>
       <div className="flex m-5">
         <div className="h-fit">
-          <Image src={imgData} alt={title} width={2000} />
+          <Image src={imgData} alt={name} width={2000} />
         </div>
         <CenterText className="m-5 p-5">{children}</CenterText>
       </div>
---
+++ app/about/ministers/MinisterCardItems.ts
@@ -0,0 +1,17 @@
+import dakota from '@/public/img/ministers/dakota.jpeg';
+import isaiah from '@/public/img/ministers/isaiah.jpeg';
+
+export const ministerCardItems = [
+    {
+        name:'Dakota Jenkins',
+        title:'Minister',
+        img:dakota,
+        data:"Dakota Jenkins is currently serving as the part-time minister. He is a Freed-Hardeman University graduate with a major in Bible, minor in Spanish, and another minor in Business. When he is not preaching at Juno, he is running his electrician business, Jenkins Electric, or studying to complete the masters degree he is currently pursuing from FHU. He is married to Adrianna Jenkins and they enjoy and appreciate the opportunity to work with the Juno congregation."
+    },
+    {
+        name:'Isaiah Leininger',
+        title:'Associate Minister',
+        img:isaiah,
+        data:"Isaiah Leininger is currently serving as our youth minister. He is a student at Freed-Hardeman University where he is double majoring in Bible and Social Work. Originally from West Virginia, he is very grateful for the opportunities he has been given to attend FHU and work with the Juno congregation. His favorite verse is Romans 8:18"
+    }
+]
