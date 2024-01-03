488e35a9eb41f6080dc5f676af20bd3968a18dc3
Evan Vance <72946268+evandvance@users.noreply.github.com>
2024-01-03 10:10:40 -0700

    Added Submission Event to form

--- app/connect/ContactForm.tsx
+++ app/connect/ContactForm.tsx
@@ -1,35 +1,77 @@
+'use client';
 import React from 'react';
 import CenterText from '../components/CenterText';
+import { FieldValues, useForm } from 'react-hook-form';
 
 interface ContactFormProps {
   className?: string;
 }
 
 const ContactForm = ({ className }: ContactFormProps) => {
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data: FieldValues) => {
+    console.log(data);
+  };
+
   return (
-    <form className={'flex flex-col ' + className}>
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className={'flex flex-col ' + className}
+    >
       <CenterText className="flex-col mb-5">
         <h2 className="text-2xl font-bold mb-2">Connect With Us</h2>
         <p className="text-lg">Contact us with your questions or inquiries</p>
       </CenterText>
       <div className="m-2 flex justify-between w-96">
         <div>
-          <h3 className="mb-1">First Name</h3>
-          <input className="rounded h-7" type="text" />
+          <label htmlFor="firstName" className="mb-1">
+            First Name
+          </label>
+          <input
+            {...register('firstName')}
+            id="firstName"
+            className="rounded h-7"
+            type="text"
+          />
         </div>
         <div>
-          <h3 className="mb-1">Last Name</h3>
-          <input className="rounded h-7" type="text" />
+          <label htmlFor="lastName" className="mb-1">
+            Last Name
+          </label>
+          <input
+            {...register('lastName')}
+            id="lastName"
+            className="rounded h-7"
+            type="text"
+          />
         </div>
       </div>
       <div className="m-2 w-96">
-        <h3 className="mb-1">Email</h3>
-        <input className="w-full rounded h-7" type="text" />
+        <label htmlFor="email" className="mb-1">
+          Email
+        </label>
+        <input
+          {...register('email')}
+          id="email"
+          className="w-full rounded h-7"
+          type="text"
+        />
       </div>
       <div className="m-2 w-96">
-        <h3 className="mb-1">Comments</h3>
-        <input className="w-full h-40 rounded" type="text" />
+        <label htmlFor="comments" className="mb-1">
+          Comments
+        </label>
+        <input
+          {...register('comments')}
+          id="comments"
+          className="w-full h-40 rounded"
+          type="text"
+        />
       </div>
+      <button type="submit" className="btn btn-primary m-2">
+        Submit
+      </button>
     </form>
   );
 };
