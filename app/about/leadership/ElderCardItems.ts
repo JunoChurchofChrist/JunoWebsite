6fc6b47ebfd596d9a0d39d7188ef61ffa6d49c7c
Evan Vance <72946268+evandvance@users.noreply.github.com>
2024-01-04 19:56:08 -0700

    Added elder cards

---
+++ app/about/leadership/ElderCardItems.ts
@@ -0,0 +1,19 @@
+import dakota from '@/public/img/ministers/dakota.jpeg'
+
+export const elderCardItems = [
+    {
+        name:'Bob Hunt',
+        contact:"Phone:(123)456-6777 email:example@email.com",
+        img:dakota
+    },
+    {
+        name:'Jimmy Allen',
+        contact:"Phone:(123)456-6777 email:example@email.com",
+        img:dakota
+    },
+    {
+        name:'Greg Bird',
+        contact:"Phone:(123)456-6777 email:example@email.com",
+        img:dakota
+    }
+]
--- app/about/leadership/page.tsx
+++ app/about/leadership/page.tsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import CenterText from '@/app/components/CenterText';
+import InfoCard from '@/app/components/InfoCard';
+import { elderCardItems } from './ElderCardItems';
 
 const LeadershipPage = () => {
-  return <div>LeadershipPage</div>;
+  return (
+    <>
+      <CenterText>
+        <p>Find a verse discribing the role of elders</p>
+        <h2 className="font-2xl text-bold">Our Elders</h2>
+      </CenterText>
+      <CenterText className="flex-wrap">
+        {elderCardItems.map((card) => (
+          <InfoCard className="p-4" imgData={card.img} title={card.name}>
+            {card.contact}
+          </InfoCard>
+        ))}
+      </CenterText>
+    </>
+  );
 };
 
 export default LeadershipPage;
