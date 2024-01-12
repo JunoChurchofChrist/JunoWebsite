59e8edd5bfff59eeb2534853c6c89bd4b7bebaa0
djyarnell0 <djyarnell@hotmail.com>
2024-01-11 20:59:59 -0600

    Added facebook integration

--- app/media/components/facebookPostWatcherCard.tsx
+++ app/media/components/facebookPostWatcherCard.tsx
@@ -1,10 +1,44 @@
+"use client";
+import React from "react";
+import ReactPlayer from "react-player";
+export const FacebookPage = () => {
+  return (
+    <div className="gap-1 columns-3">
+      <div className="liveFeed flex justify-center items-center">
+        <iframe
+          src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FJunoChurchOfChrist&tabs=timeline&width=400&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
+          width="400"
+          height="500"
+          allowFullScreen={true}
+          data-show-posts="true"
+          allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
+        ></iframe>
+      </div>
 
-<html lang="en">
-<head>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>FacebookCard</title>
-</head>
-<body>
-    
-</body>
-</html>
+      <div className="fbLink  flex justify-center items-center h-12">
+        <iframe
+          className="border-none overflow-hidden"
+          src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FJunoChurchOfChrist&tabs=timeline&width=250&height=70&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
+          width="250"
+          height="70"
+          allowFullScreen={true}
+          allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
+        ></iframe>  
+      </div>
+
+      <ReactPlayer url="https://fb.watch/pwoC2nJEIZ/" controls facebook/>
+
+
+      <div className="fbPosts">
+        <iframe
+          src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2FJunoChurchOfChrist%2Fposts%2Fpfbid093wbEdvoget1bW7KRt95rokHuxrc3Ku1bQibu4LadpdUfDKkazeQ33dmnfuL7YaCl&show_text=true&width=500"
+          width="500"
+          height="201"
+          className="border-none overflow-hidden"
+          allowFullScreen={true}
+          allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
+        ></iframe>
+      </div>
+    </div>
+  );
+};
--- app/media/page.tsx
+++ app/media/page.tsx
@@ -1,7 +1,20 @@
-import React from 'react';
+import React from "react";
+import { FacebookPage } from "./components/facebookPostWatcherCard";
+import CenterText from "../components/CenterText";
+
 
 const MediaPage = () => {
-  return <div>MediaPage</div>;
+  return (
+    <>
+      <div className="title flex justify-center items-center">
+        <p className="text-4xl">Media</p>
+      </div>
+      <CenterText>
+        <hr className="w-5/6 justify-center items-center py-12" />
+      </CenterText>
+      <FacebookPage />
+    </>
+  );
 };
 
 export default MediaPage;
