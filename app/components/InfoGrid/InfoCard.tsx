8dee8ee09b593106126fc37a9918d5f083649f3f
evandvance <evandvance@gmail.com>
2024-02-16 23:16:21 -0600

    Fixed a lack of key error

--- app/components/InfoGrid/InfoCard.tsx
+++ app/components/InfoGrid/InfoCard.tsx
@@ -19,9 +19,17 @@ const InfoCard = ({
         'flex-col h-[580px] w-96 border-t-2 justify-stretch ' + className
       }
     >
-      <Image className="my-2" src={imgData} alt={title} height={300}></Image>
-      <h2 className="p-2 text-2xl font-bold">{title}</h2>
-      <p>{data}</p>
+      <Image
+        className="my-2"
+        src={imgData}
+        alt={title}
+        height={300}
+        key={title}
+      ></Image>
+      <h2 key={title} className="p-2 text-2xl font-bold">
+        {title}
+      </h2>
+      <p key={title}>{data}</p>
     </CenterText>
   );
 };
